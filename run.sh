2e043d924c820f77b704dbdb8d7d222272dbe4f1
Shehu-Fatiudeen Lawal <shehufatiudeen@gmail.com>
2023-10-16 23:59:12 +0100

    update: changed erc to public

--- run.sh
+++ run.sh
@@ -7,6 +7,35 @@ if [ "$#" -ne 1 ]; then
   exit 1
 fi
 
+if [ -z "$aws_access_key" ] || [ -z "$aws_secret_key" ]; then
+  echo "AWS credentials (aws_access_key and aws_secret_key) are missing. Please provide them."
+  exit 1
+fi
+
+# Check for AWS CLI
+if ! command -v aws &>/dev/null; then
+  echo "AWS CLI is not installed. Please install it before running this script."
+  exit 1
+fi
+
+# Check for kubectl
+if ! command -v kubectl &>/dev/null; then
+  echo "kubectl is not installed. Please install it before running this script."
+  exit 1
+fi
+
+# Check for Helm
+if ! command -v helm &>/dev/null; then
+  echo "Helm is not installed. Please install it before running this script."
+  exit 1
+fi
+
+# Check for Docker
+if ! command -v docker &>/dev/null; then
+  echo "Docker is not installed. Please install it before running this script."
+  exit 1
+fi
+
 cd "$TF_DIR"
 
 if [ "$1" = "deploy" ]; then
@@ -15,7 +44,7 @@ if [ "$1" = "deploy" ]; then
 
   terraform plan -out=../plan.tfstate
 
-  terraform apply "../plan.tfstate"
+  terraform apply "../plan.tfstate" -var="aws_access_key=$aws_access_key" -var="aws_secret_key=aws_secret_key"
 elif [ "$1" = "clean" ]; then
 
   if [ -f "../plan.tfstate" ]; then
